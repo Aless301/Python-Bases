a3d71f36d47b43416b2d1736c5f0dc1c5d256ea9
Ora301 <149436401+Ora301@users.noreply.github.com>
2024-11-13 16:57:02 +0100

    Small Start

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello World (Fonction)")
+
+# Fin de fonction, laisse deux lignes vides
+print("Hello World")
+main()
---
+++ variable.py
@@ -0,0 +1,10 @@
+def getInfo():
+    # str var userName set by the user
+    userName = input("What's your name ? ")
+    # int var age set by the user
+    age = input("How old are you ? ")
+    # show the value of userName and age in a sentence
+    print("Hello " + userName + ", you are " + str(age) + " years old.")
+
+# end of function, let 2 lines blank
+getInfo()
